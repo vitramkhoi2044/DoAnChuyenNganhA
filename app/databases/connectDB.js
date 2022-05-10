487f7fce75d9c96a45195a1485ecc99253d4fc72
vitramkhoi2044 <vi.tk2044@sinhvien.hoasen.edu.vn>
2022-05-10 20:16:06 +0700

    update connect db

--- app/databases/connectDB.js
+++ app/databases/connectDB.js
@@ -1,16 +1,16 @@
 var mysql = require('mysql');
 
-var connection = new mysql.createConnection({
+var connection = new mysql.createPool({
     host: '103.253.147.116',
     user: 'sonkk',
     port: '3306',
     password: 'Sonkk@123',
     database: 'Do_An_Chuyen_Nganh_A',
-    charset: 'utf8_general_ci'
+    charset : 'utf8_general_ci'
 });
 
-connection.connect(function (err) {
-    if (err) { console.log("Connect Database Fail !!!" + err) }
+connection.getConnection(function(err){
+    if (err) {console.log("Connect Database Fail !!!"+err)}
 });
 
 module.exports = connection;
