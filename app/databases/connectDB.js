4b837e147687976d06395e9dce1093ba0bacf27b
vitramkhoi2044 <vi.tk2044@sinhvien.hoasen.edu.vn>
2022-05-30 15:13:54 +0700

    update connectDB

--- app/databases/connectDB.js
+++ app/databases/connectDB.js
@@ -1,10 +1,10 @@
 var mysql = require('mysql');
 
 var connection = new mysql.createPool({
-    host: '103.253.147.116',
-    user: 'sonkk',
+    host: '104.248.145.103',
+    user: 'ViTK',
     port: '3306',
-    password: 'Sonkk@123',
+    password: 'ViTK@123',
     database: 'Do_An_Chuyen_Nganh_A',
     charset: 'utf8_general_ci'
 });
